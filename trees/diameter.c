585466e3b5b016295af87dac825c91aa69b81be8
DeepanwitaR <deepanwita23297@gmail.com>
2019-05-28 22:14:41 +0800

    Create diameter.c

---
+++ trees/diameter.c
@@ -0,0 +1,28 @@
+/*This is a function problem.You only need to complete the function given below*/
+/* Tree node structure  used in the program
+ struct Node
+ {
+     int data;
+     Node* left, *right;
+}; */
+/* Computes the diameter of binary tree with given root.  */
+typedef struct Node* node;
+int max(int a, int b){
+    if(a>b)
+        return a;
+    else
+        return b;
+}
+int height(node root) {
+    if(root==NULL) {
+        return -1;
+    }
+    return 1+max(height(root->left),height(root->right));
+}
+int diameter(Node* node)
+{
+   if(node==NULL)
+        return -1;
+    else 
+        return 2+(height(node->right))+height((node->left));
+}
